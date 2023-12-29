5ab96cb1fc8e226328026e8891b094cbc68c53a1
maxvojcuk <maksimvojcuk09@gmail.com>
2023-12-29 13:15:55 +0200

    дз3

---
+++ js/modal.js
@@ -0,0 +1,17 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+    backdrop: document.querySelector("[data-backdrop]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.backdrop.classList.toggle("is-hidden");
+    refs.modal.classList.toggle("effect");
+    document.body.classList.toggle("no-scroll");
+  }
+})();
